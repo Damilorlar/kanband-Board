229b728518b424937505ca4939edb6e22267ccd6
Damilorlar <barakahmuhammad422@gmail.com>
2025-08-31 14:40:14 +0100

    first modification

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const lists = document.querySelectorAll(".list")
 
 for (const card of cards){
     card.addEventListener("dragstart", dragStart)
-    card.addEventListener("dragend", dragEnd)
+    card.addEventListener("dragend", dragEnd);
 }
 
 for (const list of lists){
